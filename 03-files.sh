d7609e84a6b2946a966f64b344a261f4e51fdae6
Adnan Saad <the.lone.comrade.was.here@gmail.com>
2024-12-29 17:05:45 +1100

    Update 03-files.sh

--- 03-files.sh
+++ 03-files.sh
@@ -1,105 +1,102 @@
-#! /bin/bash
+#!/bin/bash
 
-## explode immediately if something breaks
+## Exit immediately if something breaks
 set -e
 
 ############
-# this script copies any pre-made files to the build.
-# some sed lines will customize the files to this specific build
+# This script copies pre-made files to the build.
+# Some sed lines will customize the files to this specific build.
 ############
 
-## profile
-#cp -v $SOV_FILES/bash_profile $SOV_DIR/root/.bash_profile
+copy_files() {
+  # A helper function to copy files with verbosity
+  cp -v "$@"
+}
 
-########
-# systemd
-########
-## for systemd-networkd
-cp -v $SOV_FILES/10-dhcp.network $SOV_FILES/20-wifi.network $SOV_DIR/etc/systemd/network/
-## default to using vendor services
-cp -v $SOV_FILES/networkd.conf $SOV_DIR/etc/systemd/
-## drop the LFS supplied version and let systemd do the work
+create_symlinks() {
+  # A helper function to create symlinks
+  ln -sf "$@"
+}
+
+##########
+# Systemd
+##########
+copy_files $SOV_FILES/10-dhcp.network $SOV_FILES/20-wifi.network $SOV_DIR/etc/systemd/network/
+copy_files $SOV_FILES/networkd.conf $SOV_DIR/etc/systemd/
 rm -vf $SOV_DIR/etc/resolv.conf
-## let systemd manage the network
-cp -v $SOV_FILES/hosts $SOV_DIR/etc/
-## for systemd-sysupdate
-cp -v $SOV_FILES/10-usr.conf $SOV_FILES/30-efi.conf $SOV_DIR/etc/sysupdate.d/
-## presets for installed unit files
-cp -v $SOV_FILES/soviet.preset $SOV_DIR/etc/systemd/system-preset/
+copy_files $SOV_FILES/hosts $SOV_DIR/etc/
+copy_files $SOV_FILES/10-usr.conf $SOV_FILES/30-efi.conf $SOV_DIR/etc/sysupdate.d/
+copy_files $SOV_FILES/soviet.preset $SOV_DIR/etc/systemd/system-preset/
 
-## for the UKIs
-cp -v $SOV_FILES/cmdline $SOV_FILES/cmdline-installer $SOV_DIR/etc/kernel/
-## the loader config for systemd-boot
-cp -v $SOV_FILES/loader.conf $SOV_DIR/efi/loader/
+##########
+# UKIs and Boot
+##########
+copy_files $SOV_FILES/cmdline $SOV_FILES/cmdline-installer $SOV_DIR/etc/kernel/
+copy_files $SOV_FILES/loader.conf $SOV_DIR/efi/loader/
 echo type2 >> $SOV_DIR/efi/loader/entries.srel
-## logo for boot :)
-cp -v $SOV_FILES/logo-soviet-boot.bmp $SOV_DIR/efi
+copy_files $SOV_FILES/logo-soviet-boot.bmp $SOV_DIR/efi
 
-## zram support
-#cp -v $SOV_FILES/zramswap.conf $SOV_DIR/etc/
-#cp -v $SOV_FILES/zramctl $SOV_DIR/etc/systemd/system/
-#cp -v $SOV_FILES/zramswap.service $SOV_DIR/usr/lib/systemd/system/zramswap.service
+##########
+# ZRAM (Commented out for now)
+##########
+# copy_files $SOV_FILES/zramswap.conf $SOV_DIR/etc/
+# copy_files $SOV_FILES/zramctl $SOV_DIR/etc/systemd/system/
+# copy_files $SOV_FILES/zramswap.service $SOV_DIR/usr/lib/systemd/system/zramswap.service
 
-## lvm
-cp -v $SOV_FILES/11-dm-initramfs.rules $SOV_DIR/etc/udev/rules.d/
+##########
+# LVM
+##########
+copy_files $SOV_FILES/11-dm-initramfs.rules $SOV_DIR/etc/udev/rules.d/
 
-########
-# /etc files
-########
-## fstab - maybe not needed?
-#cp -v $SOV_FILES/fstab-install $SOV_DIR/etc/
-## profile
-cp -v $SOV_FILES/profile $SOV_DIR/etc/
-## shells
-cp -v $SOV_FILES/shells $SOV_DIR/etc/
-## inputrc
-cp -v $SOV_FILES/inputrc $SOV_DIR/etc/
-## locale default to C.UTF-8
-cp -v $SOV_FILES/locale.conf $SOV_DIR/etc
+##########
+# /etc Files
+##########
+# Commented out as unsure about necessity
+# copy_files $SOV_FILES/fstab-install $SOV_DIR/etc/
+copy_files $SOV_FILES/profile $SOV_DIR/etc/
+copy_files $SOV_FILES/shells $SOV_DIR/etc/
+copy_files $SOV_FILES/inputrc $SOV_DIR/etc/
+copy_files $SOV_FILES/locale.conf $SOV_DIR/etc/
 
-## pam
-cp -v $SOV_FILES/system-{account,auth,password,session,user} $SOV_DIR/etc/pam.d/
-cp -v $SOV_FILES/{other,login,systemd-user} $SOV_DIR/etc/pam.d/
+# PAM
+copy_files $SOV_FILES/system-{account,auth,password,session,user} $SOV_DIR/etc/pam.d/
+copy_files $SOV_FILES/{other,login,systemd-user} $SOV_DIR/etc/pam.d/
 
-# p11-kit fixes
-cp -v $SOV_FILES/trust-extract-compat $SOV_DIR/usr/libexec/p11-kit/trust-extract-compat
+# P11-kit fixes
+copy_files $SOV_FILES/trust-extract-compat $SOV_DIR/usr/libexec/p11-kit/trust-extract-compat
 
-# log files
-#touch /var/log/{btmp,lastlog,faillog,wtmp}
-#chgrp -v utmp /var/log/lastlog
-#chmod -v 664 /var/log/lastlog
-#chmod -v 600 /var/log/btmp
+# Log files
+# touch /var/log/{btmp,lastlog,faillog,wtmp}
+# chgrp -v utmp /var/log/lastlog
+# chmod -v 664 /var/log/lastlog
+# chmod -v 600 /var/log/btmp
 
-## copy and update content of os-release
-cp -v $SOV_FILES/os-release $SOV_DIR/usr/lib/
+# Update os-release
+copy_files $SOV_FILES/os-release $SOV_DIR/usr/lib/
 sed -i "s/xxxxxx/$BUILD/" $SOV_DIR/usr/lib/os-release
-## os-release and localtime should be relative symlinks, so
-## remove default files, re-link with custom work
-(cd $SOV_DIR/etc
-rm -f os-release localtime
-ln -s ../usr/lib/os-release
-ln -s ../usr/share/zoneinfo/UTC localtime
-## remove lsb-release, supplanted by os-release
-rm -vf lsb-release
+(
+  cd $SOV_DIR/etc
+  rm -f os-release localtime
+  create_symlinks ../usr/lib/os-release os-release
+  create_symlinks ../usr/share/zoneinfo/UTC localtime
+  rm -vf lsb-release
 )
 
-## installer script
-cp -v $SOV_FILES/soviet-install.sh $SOV_DIR/etc/
-cp -v $SOV_FILES/soviet-final.sh $SOV_DIR/etc/
+# Installer script
+copy_files $SOV_FILES/soviet-install.sh $SOV_DIR/etc/
+copy_files $SOV_FILES/soviet-final.sh $SOV_DIR/etc/
 chmod +x $SOV_DIR/etc/soviet-{install,final}.sh
 
-## cccp
-cp $SOV_FILES/cccp.conf $SOV_DIR/etc/
+# CCCP
+copy_files $SOV_FILES/cccp.conf $SOV_DIR/etc/
 
-## get stage 4 ready
-cp 04-config.sh  $SOV_DIR/
-# this script segfaults
-#cp $SOV_FILES/strip.sh $SOV_DIR/
+# Stage 4 preparation
+copy_files 04-config.sh $SOV_DIR/
 chmod +x $SOV_DIR/04-config.sh
-cp build $SOV_DIR/
+copy_files build $SOV_DIR/
 
-# set root password
+# Set root password
 echo 'root:sovietlinux' | chpasswd -P $SOV_DIR
 
-## all done!
+# Completion
 touch 03-complete
